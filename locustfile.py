e5d75f216fff9da8653fcc0c02209daf92da76e2
edward.van.straten <edward.van.straten@folkhalsomyndigheten.se>
2020-03-10 12:06:24 +0100

    Updated to Locust version 0.13 and current version of Hälsorapport.

--- locustfile.py
+++ locustfile.py
@@ -1,64 +1,108 @@
-from urlparse import urlparse, parse_qs
+
+
+# TODO: Remove after test
+# import pydevd_pycharm
+# Remove above
+
+from urllib.parse import urlparse, parse_qs
 from random import choice
 
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, task, between
 from locust.exception import StopLocust
 from pyquery import PyQuery
+from bs4 import BeautifulSoup
 
-NUM_USERS = 50
 
-users = ['test{0}'.format(i) for i in range(NUM_USERS)]
+NUM_USERS = 10
+
+users = ['test{0}'.format(i) for i in range(0, NUM_USERS)]
 
 remaining_users = len(users)
 
 
 class SurveyTask(TaskSet):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.token = ''
+        self.sessionid = ''
+        self.mwtoken = ''
+
     def on_start(self):
         try:
             username = users.pop()
         except IndexError:
             print('no avaliable user, continuing')
             self.interrupt(reschedule=False)
-
+        # TODO: remove after test
+        # pydevd_pycharm.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True)
+        # Remove above
+        print("user: " + username)
         response = self.client.get(
             '/accounts/login/',
             name='Login form load'
         )
-
-        csrf_token = response.cookies['csrftoken']
-
+        print(response.cookies)
+        self.token = response.cookies['csrftoken']
+        # Get the csrfmiddlewaretoken from the HMTL response
+        soup = BeautifulSoup(response.content, features="lxml")
+        self.mwtoken = soup.find('input', {'name':'csrfmiddlewaretoken'}).get('value')
+        print(self.mwtoken)
+        # TODO: remove after test
+        # import pdb;
+        # pdb.set_trace()
         login_response = self.client.post(
             '/accounts/login/',
-            {
+            headers={'X-CSRFToken': self.token},
+            data={"csrfmiddlewaretoken": self.mwtoken,
                 'username': username,
-                'password': 'password',
-                'csrfmiddlewaretoken': csrf_token
-            },
+                'password': 'password'},
+            cookies={'csrftoken': self.token},
+            allow_redirects=False,
             name='Login form submit'
         )
 
-        query_string = urlparse(login_response.url).query
-        self.client.gid = parse_qs(query_string)['gid'][0]
+        # query_string = urlparse(login_response.url).query
+        # print("query_string:" + query_string)
+        # self.sessionid = login_response.cookies['sessionid']
+        print(login_response.is_redirect)
+        print(login_response.content)
+        try:
+            self.sessionid = login_response.cookies['sessionid']
+        except KeyError:
+            print('No sessionid cookie')
+        try:
+            self.token = login_response.cookies['csrftoken']
+        except KeyError:
+            print('No csrftoken cookie')
+        # self.client.gid = parse_qs(query_string)['gid'][0]
 
         self.load_survey()
 
     @task(5)
     def load_survey(self):
         with self.client.get(
-            '/survey/show/test2/?gid='+self.client.gid,
+            '/survey/show/test2/',
+            # capture=True,
+            cookies={'csrftoken': self.token, 'sessionid': self.sessionid},
+            allow_redirects=False,
             name='Survey load',
-            capture=True
         ) as survey_response:
 
             if 'csrftoken' not in survey_response.cookies:
                 print(survey_response.status_code)
-
-            self.token = survey_response.cookies['csrftoken']
+            try:
+                self.token = survey_response.cookies['csrftoken']
+            except KeyError:
+                print('No csrftoken cookie')
+            # self.sessionid = survey_response.cookies['sessionid']
 
     @task(20)
     def save_draft(self):
+        print('SaveDraft_sessionid:', self.sessionid)
+        print('SaveDraft_token:', self.token)
         response = self.client.post(
-            '/survey/show/test2/?gid='+self.client.gid+'&draft=save',
+            '/survey/show/test2/?draft=save',
+            # data={"csrfmiddlewaretoken": self.token},
             headers={'X-CSRFToken': self.token},
             json={
                 "survey_id": 3,
@@ -76,14 +120,18 @@ class SurveyTask(TaskSet):
                     "TestMatrixEntries_multi_row3_col2": ""
                 }
             },
+            cookies={'csrftoken': self.token, 'sessionid': self.sessionid},
             name='Survey draft save'
         )
+        print('Save draft cookies: ', response.cookies)
+        print('Save draft response: ', response)
+        # self.token = response.cookies['csrftoken']
 
-    # @task(1)
+    @task(5)
     def submit_survey(self):
         global remaining_users
         response = self.client.post(
-            '/survey/show/test2/?gid='+self.client.gid,
+            '/survey/show/test2/',
             data={
                 'csrfmiddlewaretoken': self.token,
                 'PREFIL_BIRTHYEAR': '1950',
@@ -107,6 +155,7 @@ class SurveyTask(TaskSet):
                 'TestMatrixEntries_multi_row3_col1': '',
                 'TestMatrixEntries_multi_row3_col2': ''
             },
+            cookies={'crftoken': self.token, 'sessionid': self.sessionid},
             name='Survey submit'
         )
 
@@ -118,7 +167,7 @@ class SurveyTask(TaskSet):
         else:
             print('Remaining users: {0}'.format(remaining_users))
 
-        self.interrupt(reschedule=False)
+        # self.interrupt(reschedule=False)
 
 class LoginUser(TaskSet):
     @task(1)
@@ -144,6 +193,9 @@ class LoginUser(TaskSet):
             if not '/sv/valkommen' in login_response.url:
                 print('BOOM')
                 login_response.failure('Double login!')
+            print(login_response.cookies)
+            self.sessionid = login_response.cookies['sessionid']
+            self.token = login_response.cookies['csrftoken']
 
 
     @task(1)
@@ -154,5 +206,11 @@ class LoginUser(TaskSet):
 
 class WebsiteUser(HttpLocust):
     task_set = SurveyTask
-    min_wait = 1000
-    max_wait = 15000
+    wait_time = between(1, 15)
+    # min_wait = 1000
+    # max_wait = 15000
+    host = 'http://127.0.0.1:8000/'
+
+
+if __name__ == '__main__':
+    WebsiteUser().run()
